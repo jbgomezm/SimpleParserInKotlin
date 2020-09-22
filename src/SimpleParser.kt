0f3e77cc1be0612fb3e781357466c6306c896d68
Juan Bernardo Gómez Mendoza <jbgomezm@unal.edu.co>
2020-09-22 11:48:46 -0500

    Base project skeleton.

--- src/SimpleParser.kt
+++ src/SimpleParser.kt
@@ -8,5 +8,22 @@ fun consume(param: String) {
     val paramUCase = param.toUpperCase()
     if (paramUCase != param)
         println("""[W] ${param} is not in uppercase.""")
-    println("""Parameter ${paramUCase} consumed.""")
+    if (isColor(paramUCase))
+        println("""-> Color ${paramUCase} found.""")
+    if (isAnimal(paramUCase))
+        println("""-> Animal ${paramUCase} found.""")
 }
+
+fun isColor(param: String) : Boolean {
+    val validColors = listOf("BLACK", "BLUE", "YELLOW", "RED",
+        "GREEN", "WHITE")
+    return validColors.contains(param)
+}
+
+fun isAnimal(param: String) : Boolean {
+    val validAnimals = listOf("DOG", "CAT", "COW", "HORSE",
+            "FISH", "CHICKEN")
+    return validAnimals.contains(param)
+}
+
+
